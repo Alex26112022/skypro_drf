fe2f74d583a52ebb480e220935f9920fbc039069
Алексей Денисенко <elvis.presly.039127@gmail.com>
2024-08-06 16:20:59 +0300

    init project

---
+++ lms/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+from users.models import options
+
+
+class Course(models.Model):
+    title = models.CharField(max_length=255, verbose_name='Название',
+                             help_text='Укажите название', **options)
+    preview = models.ImageField(upload_to='lms/course/preview',
+                                help_text='Загрузите картинку', **options)
+    description = models.TextField(verbose_name='Описание',
+                                   help_text='Сделайте описание', **options)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Курс'
+        verbose_name_plural = 'Курсы'
+
+
+class Lesson(models.Model):
+    title = models.CharField(max_length=255, verbose_name='Название',
+                             help_text='Укажите название', **options)
+    description = models.TextField(verbose_name='Описание',
+                                   help_text='Сделайте описание', **options)
+    preview = models.ImageField(upload_to='lms/lesson/preview',
+                                help_text='Загрузите картинку', **options)
+    video = models.CharField(max_length=255, verbose_name='Ссылка на урок',
+                             **options)
+    course = models.ForeignKey(Course, on_delete=models.SET_NULL,
+                               verbose_name='Курс', related_name='lesson',
+                               **options)
---
+++ users/models.py
@@ -0,0 +1,18 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
+
+options = {'blank': True, 'null': True}
+
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True, db_index=True,
+                              verbose_name='E-mail',
+                              help_text='Укажите E-mail')
+    phone = PhoneNumberField(region='RU', verbose_name='Телефон',
+                             help_text='Укажите телефон', **options)
+    country = models.CharField(max_length=255, verbose_name='Город',
+                               help_text='Укажите страну', **options)
+    avatar = models.ImageField(upload_to='users/avatar', verbose_name='Аватар',
+                               help_text='Загрузите аватар',
+                               **options)
