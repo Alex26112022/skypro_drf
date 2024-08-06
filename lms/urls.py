88c0a58700f500ad1a267f415d98abea2e22bc5c
Алексей Денисенко <elvis.presly.039127@gmail.com>
2024-08-06 17:15:59 +0300

    add lesson api view

--- lms/urls.py
+++ lms/urls.py
@@ -1,7 +1,10 @@
+from django.urls import path
+
 from lms.apps import LmsConfig
 from rest_framework.routers import DefaultRouter
 
-from lms.views import CourseViewSet
+from lms.views import CourseViewSet, LessonCreateApiView, LessonListApiView, \
+    LessonRetrieveApiView, LessonDestroyApiView, LessonUpdateApiView
 
 app_name = LmsConfig.name
 
@@ -9,5 +12,14 @@ router = DefaultRouter()
 router.register('course', CourseViewSet, basename='course')
 
 urlpatterns = [
-
+                  path('lesson/', LessonListApiView.as_view(),
+                       name='lessons_list'),
+                  path('lesson/<int:pk>/', LessonRetrieveApiView.as_view(),
+                       name='lessons_detail'),
+                  path('lesson/create/', LessonCreateApiView.as_view(),
+                       name='lesson_create'),
+                  path('lesson/<int:pk>/update/',
+                       LessonUpdateApiView.as_view(), name='lesson_update'),
+                  path('lesson/<int:pk>/delete/',
+                       LessonDestroyApiView.as_view(), name='lesson_delete'),
               ] + router.urls
