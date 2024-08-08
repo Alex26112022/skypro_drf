e3e2325f9ff7230badc3250fa0fca93504b85ad8
Алексей Денисенко <elvis.presly.039127@gmail.com>
2024-08-08 14:59:41 +0300

    add count lesson serializer

--- lms/serializers.py
+++ lms/serializers.py
@@ -1,12 +1,17 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
 
 from lms.models import Course, Lesson
 
 
 class CourseSerializer(ModelSerializer):
+    count_lessons = SerializerMethodField()
+
     class Meta:
         model = Course
         fields = '__all__'
+
+    def get_count_lessons(self, obj):
+        return obj.lesson.count()
 
 
 class LessonSerializer(ModelSerializer):
