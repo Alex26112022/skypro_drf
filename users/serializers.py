3a60d5dc8478a98d99b01990d4bf20ebbbfb7940
Алексей Денисенко <elvis.presly.039127@gmail.com>
2024-08-08 16:43:34 +0300

    add filter and ordering payments

--- users/serializers.py
+++ users/serializers.py
@@ -1,8 +1,16 @@
 from django.contrib.auth import get_user_model
 from rest_framework.serializers import ModelSerializer
 
+from users.models import Payments
+
 
 class UserSerializer(ModelSerializer):
     class Meta:
         model = get_user_model()
         fields = ('id', 'email', 'phone', 'country', 'avatar')
+
+
+class PaymentsSerializer(ModelSerializer):
+    class Meta:
+        model = Payments
+        fields = '__all__'
