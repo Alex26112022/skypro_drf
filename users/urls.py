11a9bceeac82dd371d5ca632b9aedcc07b7ad70a
Алексей Денисенко <elvis.presly.039127@gmail.com>
2024-08-13 13:22:35 +0300

    users view refactoring

--- users/urls.py
+++ users/urls.py
@@ -1,16 +1,20 @@
 from django.urls import path
 
 from users.apps import UsersConfig
-from rest_framework.routers import DefaultRouter
 
-from users.views import UserViewSet, PaymentsListAPIView
+from users.views import UserListAPIView, UserCreateAPIView, UserDestroyAPIView, \
+    UserUpdateAPIView, UserRetrieveAPIView, PaymentsListAPIView
 
 app_name = UsersConfig.name
 
-router = DefaultRouter()
-router.register('', UserViewSet, basename='users')
-
 urlpatterns = [
-                  path('payments_list/', PaymentsListAPIView.as_view(),
-                       name='payments_list')
-              ] + router.urls
+    path('payments_list/', PaymentsListAPIView.as_view(),
+         name='payments_list'),
+    path('', UserListAPIView.as_view(), name='user_list'),
+    path('<int:pk>/', UserRetrieveAPIView.as_view(), name='user_detail'),
+    path('create/', UserCreateAPIView.as_view(), name='user_create'),
+    path('<int:pk>/update/', UserUpdateAPIView.as_view(),
+         name='user_update'),
+    path('<int:pk>/delete/', UserDestroyAPIView.as_view(),
+         name='user_delete'),
+]
--- users/views.py
+++ users/views.py
@@ -3,14 +3,48 @@ from django.shortcuts import render
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
 
-from rest_framework.viewsets import ModelViewSet
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, CreateAPIView, \
+    RetrieveAPIView, UpdateAPIView, DestroyAPIView
 
 from users.models import User, Payments
 from users.serializers import UserSerializer, PaymentsSerializer
 
 
-class UserViewSet(ModelViewSet):
+class UserListAPIView(ListAPIView):
+    user = get_user_model()
+    queryset = user.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserCreateAPIView(CreateAPIView):
+    user = get_user_model()
+    queryset = user.objects.all()
+    serializer_class = UserSerializer
+
+    def perform_create(self, serializer):
+        user = serializer.save(is_active=True)
+        user.set_password(user.password)
+        user.save()
+
+
+class UserRetrieveAPIView(RetrieveAPIView):
+    user = get_user_model()
+    queryset = user.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserUpdateAPIView(UpdateAPIView):
+    user = get_user_model()
+    queryset = user.objects.all()
+    serializer_class = UserSerializer
+
+    def perform_update(self, serializer):
+        user = serializer.save(is_active=True)
+        user.set_password(user.password)
+        user.save()
+
+
+class UserDestroyAPIView(DestroyAPIView):
     user = get_user_model()
     queryset = user.objects.all()
     serializer_class = UserSerializer
